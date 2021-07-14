a7728e2359f8f5c91bea53005d51fba68628cdf2
Sunil <sunilrk.one@gmail.com>
2021-07-14 22:04:14 +0530

    navigation bar done. SignIn and SIgnUp page almost completed with minor redirect bug

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -4,9 +4,8 @@ import { API } from '../backend'
 import Base from '../core/Base'
 
 const Home = () => {
-    console.log("API IS ",API)
     return (
-        <Base title="Home Page">
+        <Base title="Home Page" description="Welcome to MyStore">
             <div className="row">
                 <div className="col-4">
                     <button className="btn btn-success">TEST</button>
